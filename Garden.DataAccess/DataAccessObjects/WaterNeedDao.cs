6f3358f0c8da2664e303f29145dbb46a493a0c2a
Kirmie-B <kirmieb@gmail.com>
2026-08-26 14:01:47 -0400

    KRB: Fixing a few typos in WaterNeedDao and a misnamed file..

--- Garden.DataAccess/DataAccessObjects/WaterNeedDao.cs
+++ Garden.DataAccess/DataAccessObjects/WaterNeedDao.cs
@@ -11,10 +11,10 @@ namespace Garden.DataAccess.DataAccessObjects;
 public class WaterNeedDao : IWaterNeedDao
 {
     /// <summary>
-    /// Get all wter needs from the database.
+    /// Get all water needs from the database.
     /// </summary>
     /// <param name="dbTransaction">The database transaction to use for this call.</param>
-    /// <returns>A list containing all of the Water needs.</returns>
+    /// <returns>A list containing all of the water needs.</returns>
     public async Task<List<WaterNeed>> GetAllWaterNeeds(IDbTransaction dbTransaction)
     {
         var sqlQuery = $@"
@@ -22,7 +22,7 @@ public class WaterNeedDao : IWaterNeedDao
                 id AS {nameof(WaterNeed.Id)},
                 name AS {nameof(WaterNeed.Name)},
                 description AS {nameof(WaterNeed.Description)}
-            FROM Water_need";
+            FROM water_need";
 
         var waterNeeds = await dbTransaction.Connection!.QueryAsync<WaterNeed>(sqlQuery);
         return waterNeeds.ToList();
